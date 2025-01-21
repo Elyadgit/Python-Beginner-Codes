d1dd5efe1b13321b2f0a6987f18436c1265284e1
Mohammadreza Asgari <elyadgithub@gmail.com>
2025-01-21 21:56:50 +0330

    Update 05.py

--- 05.py
+++ 05.py
@@ -1,3 +1,12 @@
-x = input ()
-y = input ()
-print (ord(y)-ord(x))
+a , b =input().split()
+a = int(a)
+b = int (b)
+a = a*100
+b = b*100
+d =(a * b)
+c = 4
+f = 5
+h = c * f
+k =(d / h)
+k = int(k)
+print (k)
