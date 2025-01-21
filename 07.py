2e9bc7af821cdd220914b35fd49de3417c7c0489
Mohammadreza Asgari <elyadgithub@gmail.com>
2025-01-21 21:58:28 +0330

    shatranj

---
+++ 07.py
@@ -0,0 +1,14 @@
+shah , vazir , rokh , fil , asb , srbz = input().split()
+shah = int (shah)
+vazir = int (vazir)
+rokh = int (rokh)
+fil = int (fil)
+asb = int (asb)
+srbz = int (srbz)
+sh = 1
+v = 1
+r = 2
+f = 2
+a = 2
+s = 8
+print ((sh-shah) , (v-vazir) , (r-rokh) , (f-fil) , (a-asb) , (s-srbz))
